2597a2acf97d92501bc48c293b13981c55506280
julil20 <juugoo46@gmail.com>
2025-02-11 21:21:12 -0300

    login e uns formularios

---
+++ principal.php
@@ -0,0 +1,11 @@
+<html lang="portugues">
+<header class="header">
+    <a class="logo" href="<?=constant('URL_LOCAL_SITE_PAGINA').'principal'?>">InfoSports</a>
+    <div class="headerBtnGroup">
+    
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
