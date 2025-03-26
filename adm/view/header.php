57d26f8bcf0c71aa3868dffd7ec16c46ec218ffa
Lucasmsl00 <lucaslima131@gmail.com>
2025-03-26 00:33:27 -0300

    Adicionando login e recuperacao

--- adm/view/header.php
+++ adm/view/header.php
@@ -1,3 +1,9 @@
+<?php
+
+include_once("./config/configuracao.php");
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -5,7 +11,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Acervo Digital</title>
     <link rel="stylesheet" href="./assets/css/style.css">
+
+    <?php if ($paginaUrl === "login"):?>
+    <link rel="stylesheet" href="./assets/css/login.css">
+    <?php endif;?>
+
+    <?php if ($paginaUrl === "recuperacao"):?>
+    <link rel="stylesheet" href="./assets/css/esq.css">
+    <?php endif;?> 
+    
     <link rel="shortcut icon" href="./assets/imagens/logo.ico" type="image/x-icon">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <style>
+      @import url('https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,wght@8..144,100..1000&display=swap');
+    </style>
 
 </head>
 <body>
@@ -15,10 +36,9 @@
             <div id="menu">
                 <h2 class="center">Acervo Digital</h2>
                 <div id="menu_button">
-                <button class="button_menu"><a href="#">Home</a></button>
-                <button class="button_menu"><a href="cadastro.html">Cadastro</a></button>
+                <button class="button_menu"><a href="<?= constant('URL_LOCAL_SITE_PAGINA').'adm'?>">Home</a></button>
                 <button class="button_menu"><a href="#">Cadastrar PI</a></button>
-                <button class="button_menu"><a href="#">Login</a></button>
+                <button class="button_menu"><a href="<?= constant('URL_LOCAL_SITE_PAGINA').'login'?>">Login</a></button>
                 <button class="button_menu"><a href="#">Sair</a></button>
                 </div>
             </div>
--- index.php
+++ index.php
@@ -7,5 +7,11 @@ if($_GET && isset($_GET['pagina'])){
 }
 
 if ($paginaUrl === "adm") {
-    include_once("./adm/view/index.php");
+    include_once("./adm/index.php");
+}elseif ($paginaUrl === "login") {
+    include_once("./adm/view/login.php");
+}elseif ($paginaUrl === "recuperacao") {
+    include_once("./adm/view/recuperacao.php");
+}else {
+    echo "ERROR 404 PAGE NOT FOUND";
 }
